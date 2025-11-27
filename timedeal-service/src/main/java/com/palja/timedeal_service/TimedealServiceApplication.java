8a18f504b0a4222ebd1bfd8555ae276ad411380f
bu119 <109335452+bu119@users.noreply.github.com>
2025-11-27 15:34:01 +0900

    FEAT/init project setup (#1)
    * chore: initialize project setup
    
    * feat: setup infrastructure stack using Docker Compose
    
    * feat: add review schema
    
    * chore: add base application.yml
    
    * chore: set gateway-service port
    
    * chore: configure discovery-service as Eureka Server
    
    * chore: add Eureka client config to gateway-service
    
    * chore: add comments to infra docker-compose
    
    * chore: add service docker-compose and Dockerfiles
    
    * chore: remove EnableEurekaServer
    
    * chore: enable Eureka Server with @EnableEurekaServer
    
    * chore: apply multi-stage Dockerfile build
    
    * chore: update .gitignore for env and gradle properties

---
+++ timedeal-service/src/main/java/com/palja/timedeal_service/TimedealServiceApplication.java
@@ -0,0 +1,13 @@
+package com.palja.timedeal_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimedealServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimedealServiceApplication.class, args);
+	}
+
+}
