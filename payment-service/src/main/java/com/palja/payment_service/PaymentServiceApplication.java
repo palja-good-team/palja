d9464c3f4b9f6e7cdd35ec992e16c3a9ff046686
S2hyeyunS2 <hhyeyun22@gmail.com>
2025-11-28 16:09:20 +0900

    chore: ComponentScan을 통해 common 모듈의 Bean 등록

--- payment-service/src/main/java/com/palja/payment_service/PaymentServiceApplication.java
+++ payment-service/src/main/java/com/palja/payment_service/PaymentServiceApplication.java
@@ -2,8 +2,10 @@ package com.palja.payment_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.palja.payment_service", "com.palja.common"})
 public class PaymentServiceApplication {
 
 	public static void main(String[] args) {
