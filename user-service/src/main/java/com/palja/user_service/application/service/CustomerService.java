088e2904bafe9d0878a6709b2312f58dd044da3f
geon <leegeonhee0204@naver.com>
2025-11-29 16:36:46 +0900

    feat: 일반 사용자 생성 API 구현

--- user-service/src/main/java/com/palja/user_service/application/service/CustomerService.java
+++ user-service/src/main/java/com/palja/user_service/application/service/CustomerService.java
@@ -1,4 +1,9 @@
 package com.palja.user_service.application.service;
 
+import com.palja.user_service.application.command.CreateCustomerCommand;
+
 public interface CustomerService {
+
+	void createCustomer(CreateCustomerCommand command);
+
 }
