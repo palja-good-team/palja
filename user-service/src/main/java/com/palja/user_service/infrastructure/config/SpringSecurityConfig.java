be16b052b1d5e71cfca039a24e69bc1c03e048e1
geon <leegeonhee0204@naver.com>
2025-11-29 20:23:13 +0900

    feat: Spring Security 기본 셋팅

--- user-service/src/main/java/com/palja/user_service/infrastructure/config/SpringSecurityConfig.java
+++ user-service/src/main/java/com/palja/user_service/infrastructure/config/SpringSecurityConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,6 +14,21 @@ public class SpringSecurityConfig {
 
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.csrf(AbstractHttpConfigurer::disable)
+
+			.sessionManagement(session -> session
+				.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+			)
+
+			.formLogin(AbstractHttpConfigurer::disable)
+
+			.logout(AbstractHttpConfigurer::disable)
+
+			.authorizeHttpRequests(auth -> auth
+				.anyRequest().permitAll()
+			)
+		;
+
 		return http.build();
 	}
 
