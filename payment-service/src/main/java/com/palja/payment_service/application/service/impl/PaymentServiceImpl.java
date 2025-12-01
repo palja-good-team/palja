adf688e5aa45a694b8634c2be4899dd9a212d544
S2hyeyunS2 <hhyeyun22@gmail.com>
2025-12-01 11:54:06 +0900

    add: Payment TODO 추가

--- payment-service/src/main/java/com/palja/payment_service/application/service/impl/PaymentServiceImpl.java
+++ payment-service/src/main/java/com/palja/payment_service/application/service/impl/PaymentServiceImpl.java
@@ -25,6 +25,21 @@ public class PaymentServiceImpl implements PaymentService {
 
     @Override
     @Transactional(noRollbackFor = BusinessException.class)
+    /*TODO: order-service에서 orderId로 주문을 조회하고
+            주문금액과 결제 금액이 동일한지, userId, status(CREATED) 검증하기
+            만약, 다르다면 PG 호출하지 말고 Payment 상태값을 FAILED로 저장하고 로그 남기기
+
+      TODO: PaymentLog에 retryCount, success 추가하고
+            5~10분 이내 같은 orderId와 userId로 결제 5번 이상 실패하면
+            더 이상 PG 호출이 안되도록 막기, order-service에 이벤트 전달 (상태 CREATED->CANCLED)
+
+      TODO: 결제 성공 & 실패 이벤트 발생
+            1. PaymentApprovedEvent(orderId, paymentId, userId, amount, paymentKey 등)
+             Kafka에 order-service, coupon-service 등이 이 이벤트를 구독해서
+             주문 상태 변경, 쿠폰 사용 처리 등을 비동기로 처리
+            2. PaymentFailedEvent(orderId, paymentId, userId, 사유, 에러코드 등)
+                Kafka에 order-service가 주문 상태를 그래도 CREATED로 유지, coupon-service 도 미사용으로 유지
+     */
     public PaymentDetailRes createPayment(CreatePaymentCommand command) {
         Payment payment = command.toEntity();
         paymentRepository.save(payment);
