c839d91054119aa078dd4d3a3351297d55bcc938
newbee9507 <cho6185@naver.com>
2025-12-01 10:27:37 +0900

    Product 엔티티 id생성 전략 변경

--- product-service/src/main/java/com/palja/product_service/domain/entity/Product.java
+++ product-service/src/main/java/com/palja/product_service/domain/entity/Product.java
@@ -14,7 +14,7 @@ import java.util.UUID;
 public class Product {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.UUID)
     @Column(name = "product_id")
     private UUID id;
 
