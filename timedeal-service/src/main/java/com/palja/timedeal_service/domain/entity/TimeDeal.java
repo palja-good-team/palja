fdf3d38953f83c0ff2affc5154df7fdcf3e05d81
alscksdlek <mseon916@naver.com>
2025-12-01 14:04:43 +0900

    feat: TimeDeal 및 TimeDealStock 엔티티와 VO 추가

--- timedeal-service/src/main/java/com/palja/timedeal_service/domain/entity/TimeDeal.java
+++ timedeal-service/src/main/java/com/palja/timedeal_service/domain/entity/TimeDeal.java
@@ -1,8 +1,9 @@
 package com.palja.timedeal_service.domain.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import com.palja.timedeal_service.domain.vo.Amount;
+import com.palja.timedeal_service.domain.vo.Period;
+import com.palja.timedeal_service.domain.vo.TimeDealStatus;
+import jakarta.persistence.*;
 import lombok.*;
 
 import java.util.UUID;
@@ -16,5 +17,36 @@ import java.util.UUID;
 public class TimeDeal {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "time_deal_id")
     private UUID timeDealId;
+
+    @Column(name = "product_id", nullable = false)
+    private UUID productId;
+
+    @Column(name = "company_user_id", nullable = false)
+    private UUID companyUserId;
+
+    @Column(name = "title", nullable = false, length = 100)
+    private String title;
+
+    @Column(name = "description", nullable = false, length = 500)
+    private String description;
+
+    @Embedded
+    private Period period;
+
+    @Embedded
+    private Amount amount;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "time_deal_status", nullable = false)
+    private TimeDealStatus timeDealStatus = TimeDealStatus.PENDING;
+
+    @OneToOne(mappedBy = "timeDeal",
+            cascade = CascadeType.ALL,
+            orphanRemoval = true,
+            fetch = FetchType.LAZY
+    )
+    private TimeDealStock timeDealStock;
 }
