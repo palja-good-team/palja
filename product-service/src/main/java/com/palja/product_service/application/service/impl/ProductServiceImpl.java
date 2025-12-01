2883120f41df02b8ce160907f9f841cacd248e52
newbee9507 <cho6185@naver.com>
2025-12-01 14:07:12 +0900

    FEAT/ProductService - 상품 등록 기능 구현

--- product-service/src/main/java/com/palja/product_service/application/service/impl/ProductServiceImpl.java
+++ product-service/src/main/java/com/palja/product_service/application/service/impl/ProductServiceImpl.java
@@ -1,12 +1,17 @@
 package com.palja.product_service.application.service.impl;
 
+import com.palja.common.exception.BusinessException;
+import com.palja.common.exception.CommonErrorCode;
 import com.palja.product_service.application.command.CreateProductCommand;
 import com.palja.product_service.application.service.ProductService;
+import com.palja.product_service.domain.entity.Product;
 import com.palja.product_service.domain.repository.ProductRepository;
 import com.palja.product_service.presentation.dto.res.ProductDetailRes;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
 
 @Service
 @Transactional
@@ -17,5 +22,23 @@ public class ProductServiceImpl implements ProductService {
 
     @Override
     public ProductDetailRes createProduct(CreateProductCommand createCommand) {
+
+        /*
+          헤더로 로그인 아이디가 넘어와서, 해당 유저의 아이디를 조회해 가져와야함.
+          -> UUID.randomUUID() 부분.
+          이때, 회사의 이름을 같이 줘서 이 유저의 회사가 맞는지 검증도 같이 요청
+          userClient.요청(로그인아이디, 회사이름);
+         */
+        Product product = Product.create(createCommand.name(),
+                createCommand.description(),
+                createCommand.price(),
+                createCommand.category(),
+                UUID.randomUUID(),
+                createCommand.companyName(),
+                createCommand.stock());
+
+        Product savedProduct = repository.save(product);
+
+        return ProductDetailRes.fromEntity(savedProduct);
     }
 }
--- product-service/src/main/java/com/palja/product_service/presentation/controller/ProductController.java
+++ product-service/src/main/java/com/palja/product_service/presentation/controller/ProductController.java
@@ -1,15 +1,19 @@
 package com.palja.product_service.presentation.controller;
 
 import com.palja.common.response.ApiResponse;
+import com.palja.product_service.application.command.CreateProductCommand;
 import com.palja.product_service.application.service.ProductService;
 import com.palja.product_service.presentation.dto.req.CreateProductReq;
 import com.palja.product_service.presentation.dto.res.ProductDetailRes;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,5 +24,10 @@ public class ProductController {
 
     @PostMapping
     public ResponseEntity<ApiResponse<ProductDetailRes>> createProduct(@RequestBody CreateProductReq createReq) {
+
+        CreateProductCommand createCommand = createReq.toCommand(createReq);
+        ProductDetailRes productDetail = service.createProduct(createCommand);
+
+        return new ResponseEntity<>(ApiResponse.success(productDetail,"상품 등록 성공"), HttpStatus.OK);
     }
 }
