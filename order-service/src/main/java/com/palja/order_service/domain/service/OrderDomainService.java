3a8b13245f5dc5a05394000ada71e2f599f34eec
김부경 <sa01023@naver.com>
2025-12-02 13:17:17 +0900

    feat: domain 레이어 Service 및 Repository 스켈레톤 생성

---
+++ order-service/src/main/java/com/palja/order_service/domain/service/OrderDomainService.java
@@ -0,0 +1,17 @@
+package com.palja.order_service.domain.service;
+
+import com.palja.order_service.domain.repository.OrderItemDeliveryRepository;
+import com.palja.order_service.domain.repository.OrderItemRepository;
+import com.palja.order_service.domain.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class OrderDomainService {
+
+    private final OrderRepository orderRepository;
+    private final OrderItemRepository orderItemRepository;
+    private final OrderItemDeliveryRepository orderItemDeliveryRepository;
+
+}
