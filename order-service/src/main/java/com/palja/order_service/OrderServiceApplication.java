6bb4489cb14063af197ccfee8abe7b38a56d5340
김부경 <sa01023@naver.com>
2025-12-02 13:35:16 +0900

    feat: FeignClient 활성화를 위한 @EnableFeignClients 추가

--- order-service/src/main/java/com/palja/order_service/OrderServiceApplication.java
+++ order-service/src/main/java/com/palja/order_service/OrderServiceApplication.java
@@ -2,10 +2,12 @@ package com.palja.order_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.palja.order_service", "com.palja.common"})
+@EnableFeignClients
 public class OrderServiceApplication {
 
 	public static void main(String[] args) {
