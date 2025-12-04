b1216f302654abc76f288f2ee7acc3f1e188e92a
S2hyeyunS2 <hhyeyun22@gmail.com>
2025-12-04 20:25:11 +0900

    fix: softDelete() 삭제

--- payment-service/src/main/java/com/palja/payment_service/domain/entity/Payment.java
+++ payment-service/src/main/java/com/palja/payment_service/domain/entity/Payment.java
@@ -92,8 +92,4 @@ public class Payment extends BaseEntity {
         this.cancelReason = reason;
         this.completedAt = LocalDateTime.now();
     }
-
-    public void softDelete(){
-        super.softDelete();
-    }
 }
