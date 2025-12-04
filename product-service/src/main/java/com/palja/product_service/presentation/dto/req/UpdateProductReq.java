53cf4fbaa48376673fd16e3269bec3993813d75d
newbee9507 <cho6185@naver.com>
2025-12-04 17:02:04 +0900

    FEAT/ProductService - 상품 수정 Req 클래스에 검증 에너테이션 추가

--- product-service/src/main/java/com/palja/product_service/presentation/dto/req/UpdateProductReq.java
+++ product-service/src/main/java/com/palja/product_service/presentation/dto/req/UpdateProductReq.java
@@ -1,14 +1,21 @@
 package com.palja.product_service.presentation.dto.req;
 
 import com.palja.product_service.application.command.UpdateProductCommand;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 @Getter
 public class UpdateProductReq {
 
+    @Size(min = 2, max = 30)
     private String name;
+
     private String description;
+
+    @Positive
     private Long price;
+
     private String category;
 
     public UpdateProductCommand toCommand() {
