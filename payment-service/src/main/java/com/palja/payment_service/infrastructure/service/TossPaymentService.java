02d7564394ecd42b54866a2b50c28a4b252d8ff9
S2hyeyunS2 <hhyeyun22@gmail.com>
2025-12-06 00:36:07 +0900

    move: infrastructure/service로 이동

--- payment-service/src/main/java/com/palja/payment_service/infrastructure/service/TossPaymentService.java
+++ payment-service/src/main/java/com/palja/payment_service/infrastructure/service/TossPaymentService.java
@@ -1,4 +1,4 @@
-package com.palja.payment_service.infrastructure.external;
+package com.palja.payment_service.infrastructure.service;
 
 import com.palja.common.exception.BusinessException;
 import com.palja.payment_service.application.dto.response.PGPaymentRes;
