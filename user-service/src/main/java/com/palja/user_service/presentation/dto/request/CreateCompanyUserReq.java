2176c79a4d32b91b3fa1bc0b67c5f2315baba26c
geon <leegeonhee0204@naver.com>
2025-12-08 16:48:28 +0900

    docs: CompanyUserController Swagger 작성

--- user-service/src/main/java/com/palja/user_service/presentation/dto/request/CreateCompanyUserReq.java
+++ user-service/src/main/java/com/palja/user_service/presentation/dto/request/CreateCompanyUserReq.java
@@ -7,6 +7,7 @@ import com.palja.user_service.presentation.dto.validation.annotation.ValidLoginI
 import com.palja.user_service.presentation.dto.validation.annotation.ValidName;
 import com.palja.user_service.presentation.dto.validation.annotation.ValidPassword;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -16,24 +17,31 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class CreateCompanyUserReq {
 
+	@Schema(description = "아이디", example = "company1")
 	@ValidLoginId
 	private String loginId;
 
+	@Schema(description = "비밀번호", example = "Password123!")
 	@ValidPassword
 	private String password;
 
+	@Schema(description = "이름", example = "판매자")
 	@ValidName
 	private String name;
 
+	@Schema(description = "업체 이름", example = "업체1")
 	@NotBlank(message = "업체 이름을 입력해주세요.")
 	private String companyName;
 
+	@Schema(description = "사업자 등록 번호", example = "123-45-67890")
 	@ValidCompanyNumber
 	private String companyNumber;
 
+	@Schema(description = "이메일", example = "company1@gmail.com")
 	@ValidEmail
 	private String email;
 
+	@Schema(description = "주소", example = "서울시 강남구 테헤란로 123")
 	@NotBlank(message = "주소를 입력해주세요.")
 	private String address;
 
--- user-service/src/main/java/com/palja/user_service/presentation/dto/request/UpdateCompanyUserStatusReq.java
+++ user-service/src/main/java/com/palja/user_service/presentation/dto/request/UpdateCompanyUserStatusReq.java
@@ -2,6 +2,7 @@ package com.palja.user_service.presentation.dto.request;
 
 import com.palja.user_service.application.command.UpdateCompanyUserStatusCommand;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -11,6 +12,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class UpdateCompanyUserStatusReq {
 
+	@Schema(description = "상태", example = "active")
 	@NotBlank(message = "상태를 입력해주세요.")
 	private String status;
 
