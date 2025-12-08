9217cc1a1933a15172690497319ef31e880bf510
김부경 <sa01023@naver.com>
2025-12-09 00:35:56 +0900

    feat: Eureka Client 설정 추가

--- order-service/src/main/java/com/palja/order_service/OrderServiceApplication.java
+++ order-service/src/main/java/com/palja/order_service/OrderServiceApplication.java
@@ -2,16 +2,17 @@ package com.palja.order_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.palja.order_service", "com.palja.common"})
 @EnableFeignClients
+@EnableDiscoveryClient
 public class OrderServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OrderServiceApplication.class, args);
 	}
-
 }
