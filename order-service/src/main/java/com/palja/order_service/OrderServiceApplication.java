a00f75fe9e463ffb6f26dc726169ffe35ae792a2
김부경 <sa01023@naver.com>
2025-12-09 10:53:29 +0900

    feat: JPA Auditing 설정 추가

--- order-service/src/main/java/com/palja/order_service/OrderServiceApplication.java
+++ order-service/src/main/java/com/palja/order_service/OrderServiceApplication.java
@@ -5,11 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.palja.order_service", "com.palja.common"})
 @EnableFeignClients
 @EnableDiscoveryClient
+@EnableJpaAuditing
 public class OrderServiceApplication {
 
 	public static void main(String[] args) {
