3aff3eb7c21bc74a2a28efe7316f55a5237e60ca
alscksdlek <mseon916@naver.com>
2025-12-09 20:51:28 +0900

    chore: 타임딜 서비스 Swagger 설정 및 공통 모듈 버전 업데이트

--- timedeal-service/src/main/java/com/palja/timedeal_service/TimedealServiceApplication.java
+++ timedeal-service/src/main/java/com/palja/timedeal_service/TimedealServiceApplication.java
@@ -2,6 +2,7 @@ package com.palja.timedeal_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -9,6 +10,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.palja.timedeal_service", "com.palja.common"})
 @EnableJpaAuditing
+@EnableDiscoveryClient
 @EnableFeignClients
 public class TimedealServiceApplication {
 
