5c972f82d619d920c24819ad598aae826339fef7
newbee9507 <cho6185@naver.com>
2025-12-09 22:01:42 +0900

    REFACTOR/ProductService - @EnableDiscoveryClient명시, 로드밸런싱 의존성 추가

--- product-service/src/main/java/com/palja/product_service/ProductServiceApplication.java
+++ product-service/src/main/java/com/palja/product_service/ProductServiceApplication.java
@@ -2,6 +2,7 @@ package com.palja.product_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -11,6 +12,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableJpaAuditing
 @EnableScheduling
 @EnableFeignClients
+@EnableDiscoveryClient
 @ComponentScan(basePackages = {"com.palja.product_service", "com.palja.common"})
 public class ProductServiceApplication {
 
