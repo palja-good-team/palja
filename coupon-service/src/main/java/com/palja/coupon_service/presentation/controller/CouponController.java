e5ce9406f65da42914001cebcd07fd213bd272ef
bitamin707 <bitamin707@naver.com>
2025-12-10 16:23:58 +0900

    refac:Swagger Schema 적용

--- coupon-service/src/main/java/com/palja/coupon_service/presentation/controller/CouponController.java
+++ coupon-service/src/main/java/com/palja/coupon_service/presentation/controller/CouponController.java
@@ -9,7 +9,10 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -58,7 +61,12 @@ public interface CouponController {
             summary = "쿠폰 목록 조회",
             description = "사용자의 보유 쿠폰 목록을 조회합니다."
     )
-    ResponseEntity<ApiResponse<PageResponse<ReadCouponUserRes>>> getCouponList(Pageable pageable);
+    ResponseEntity<ApiResponse<PageResponse<ReadCouponUserRes>>> getCouponList(@ParameterObject
+                                                                               @PageableDefault(
+                                                                                       size = 20,
+                                                                                       sort = "createdAt",
+                                                                                       direction = Sort.Direction.DESC)
+                                                                               Pageable pageable);
 
     @Operation(
             summary = "쿠폰 상세 조회",
