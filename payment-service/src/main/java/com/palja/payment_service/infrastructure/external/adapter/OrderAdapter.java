aeaa6dcbc2f0da3d9bed572ae271a065b611b35e
S2hyeyunS2 <hhyeyun22@gmail.com>
2025-12-11 21:14:58 +0900

    fix: OrderAdapter 주석처리

--- payment-service/src/main/java/com/palja/payment_service/infrastructure/external/adapter/OrderAdapter.java
+++ payment-service/src/main/java/com/palja/payment_service/infrastructure/external/adapter/OrderAdapter.java
@@ -2,9 +2,8 @@ package com.palja.payment_service.infrastructure.external.adapter;
 
 import com.palja.payment_service.application.dto.response.OrderRes;
 import com.palja.payment_service.application.service.OrderService;
-import com.palja.payment_service.infrastructure.external.OrderClient;
+// import com.palja.payment_service.infrastructure.external.OrderClient; // TODO: order-service 연동 시 사용
 import com.palja.payment_service.infrastructure.external.dto.response.OrderDTO;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
@@ -12,10 +11,9 @@ import java.util.UUID;
 
 @Slf4j
 @Component
-@RequiredArgsConstructor
 public class OrderAdapter implements OrderService {
 
-    // TODO: 주문 서비스 연동 시 orderClient 주입
+    // TODO: 주문 서비스 연동 시 orderClient 주입 및 @RequiredArgsConstructor 추가
     //private final OrderClient orderClient;
 
     @Override
