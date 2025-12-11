e3c34e619df42790a20c15fc57750cd6f46a58ac
geon <leegeonhee0204@naver.com>
2025-12-11 13:06:28 +0900

    move: LoggingAspect를 infrastructure 계층으로 이동

--- user-service/src/main/java/com/palja/user_service/infrastructure/aop/LoggingAspect.java
+++ user-service/src/main/java/com/palja/user_service/infrastructure/aop/LoggingAspect.java
@@ -1,4 +1,4 @@
-package com.palja.user_service.application.aop;
+package com.palja.user_service.infrastructure.aop;
 
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
