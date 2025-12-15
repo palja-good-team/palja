80b292f3e0a8555b4377e6b69d20287a18d2a9ac
S2hyeyunS2 <hhyeyun22@gmail.com>
2025-12-15 14:25:24 +0900

    feat: PaymentService에 order-service 결제 완료 API 연동

--- payment-service/src/main/java/com/palja/payment_service/application/port/OrderClient.java
+++ payment-service/src/main/java/com/palja/payment_service/application/port/OrderClient.java
@@ -2,8 +2,11 @@ package com.palja.payment_service.application.port;
 
 import com.palja.payment_service.application.dto.external.OrderRes;
 
+import java.math.BigDecimal;
 import java.util.UUID;
 
 public interface OrderClient {
     OrderRes getOrderByOrderId(UUID orderId);
+
+    void completeOrderPayment(UUID orderId, UUID paymentId, BigDecimal paidAmount);
 }
