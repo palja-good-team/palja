ec748e033b1b88b01b65b25870ff20f6c15f59d7
김부경 <sa01023@naver.com>
2025-12-16 01:50:14 +0900

    feat: 비동기 처리 활성화를 위한 Async 설정 추가

---
+++ order-service/src/main/java/com/palja/order_service/infrastructure/config/AsyncConfig.java
@@ -0,0 +1,9 @@
+package com.palja.order_service.infrastructure.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@EnableAsync
+@Configuration
+public class AsyncConfig {
+}
