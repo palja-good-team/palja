8e9640f01f13bbc1baaf053909de80c5bf87fd4c
S2hyeyunS2 <hhyeyun22@gmail.com>
2025-12-17 11:29:47 +0900

    rename: 도메인+ ClientAdapter로 수정

--- payment-service/src/main/java/com/palja/payment_service/infrastructure/external/adapter/OrderClientAdapter.java
+++ payment-service/src/main/java/com/palja/payment_service/infrastructure/external/adapter/OrderClientAdapter.java
@@ -19,7 +19,7 @@ import java.util.UUID;
 @Slf4j
 @Component
 @RequiredArgsConstructor
-public class OrderAdapter implements OrderClient {
+public class OrderClientAdapter implements OrderClient {
 
     private final OrderFeignClient orderFeignClient;
 
