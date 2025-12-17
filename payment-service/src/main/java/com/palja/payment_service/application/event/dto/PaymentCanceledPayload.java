b730163a91046ab8c2b7e7e284d5a636953b8717
S2hyeyunS2 <hhyeyun22@gmail.com>
2025-12-17 23:31:32 +0900

    feat: Payment event payload dto 추가

---
+++ payment-service/src/main/java/com/palja/payment_service/application/event/dto/PaymentCanceledPayload.java
@@ -0,0 +1,21 @@
+package com.palja.payment_service.application.event.dto;
+
+import lombok.Builder;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Builder
+public record PaymentCanceledPayload(
+        UUID paymentId,
+        UUID orderId,
+        Long userId,
+        BigDecimal canceledAmount,
+        String cancelReason,
+        String paymentKey,
+        String pgResponseCode,
+        String pgResponseMessage,
+        LocalDateTime canceledAt
+) {
+}
