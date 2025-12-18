3b089489ce76b096791e9112e75b961e4e71eaa0
newbee9507 <cho6185@naver.com>
2025-12-18 12:45:44 +0900

    테스트35

--- review-service/src/main/java/com/palja/review_service/presentation/controller/ReviewController.java
+++ review-service/src/main/java/com/palja/review_service/presentation/controller/ReviewController.java
@@ -28,7 +28,7 @@ public class ReviewController {
 
     @PostMapping
     public ResponseEntity<ApiResponse<CreateReviewRes>> createReview(@RequestBody @Valid CreateReviewReq createReq) {
-        System.out.println("asdf");
+        System.out.println("adsdf");
         String loginId = AuditorContext.get().getLoginId();
         if(createReq.getIsLike().equals(Boolean.TRUE) && createReq.getIsLike().equals(createReq.getDisLike()))
             throw new IllegalArgumentException();
