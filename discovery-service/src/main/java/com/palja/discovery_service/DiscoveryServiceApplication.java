6bcdd59a919c2600ed47d6539886241c84c3fbda
newbee9507 <cho6185@naver.com>
2025-12-19 17:18:11 +0900

    테스트37

--- discovery-service/src/main/java/com/palja/discovery_service/DiscoveryServiceApplication.java
+++ discovery-service/src/main/java/com/palja/discovery_service/DiscoveryServiceApplication.java
@@ -10,7 +10,7 @@ public class DiscoveryServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DiscoveryServiceApplication.class, args);
-        System.out.println("asdfffffdffd");
+        System.out.println("asdfffffdfdfd");
 	}
 
 }
