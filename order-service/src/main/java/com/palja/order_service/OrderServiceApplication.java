a02f4214934c53f1c5592ae1151ffed050e23328
newbee9507 <cho6185@naver.com>
2025-12-20 20:58:21 +0900

    테스트aaaaaaaabbc

--- order-service/src/main/java/com/palja/order_service/OrderServiceApplication.java
+++ order-service/src/main/java/com/palja/order_service/OrderServiceApplication.java
@@ -15,6 +15,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class OrderServiceApplication {
 
 	public static void main(String[] args) {
+        System.out.println("a");
 		SpringApplication.run(OrderServiceApplication.class, args);
 	}
 }
