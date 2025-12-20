5d06da4927e36f66c81111232fcbca3e28534865
bitamin707 <bitamin707@naver.com>
2025-12-20 21:54:01 +0900

    feat:kafka 쿠폰 취소 구현

--- coupon-service/src/main/java/com/palja/coupon_service/infrastructure/external/kafka/KafkaTopics.java
+++ coupon-service/src/main/java/com/palja/coupon_service/infrastructure/external/kafka/KafkaTopics.java
@@ -4,4 +4,8 @@ public class KafkaTopics {
     public static final String COUPON_USE_REQUEST = "order.coupon.use.request";
     public static final String COUPON_USE_SUCCESS = "order.coupon.use.success";
     public static final String COUPON_USE_FAILURE = "order.coupon.use.failure";
+
+    public static final String COUPON_CANCEL_REQUEST = "order.coupon.cancel.request";
+    public static final String COUPON_CANCEL_SUCCESS = "order.coupon.cancel.success";
+    public static final String COUPON_CANCEL_FAILURE = "order.coupon.cancel.failure";
 }
