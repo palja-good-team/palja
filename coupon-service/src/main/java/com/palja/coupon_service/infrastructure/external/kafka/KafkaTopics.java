10adb7baeeb8ae5b81d4dd70bf62bc62528a04e8
bitamin707 <bitamin707@naver.com>
2025-12-20 13:58:16 +0900

    feat:kafka 쿠폰 사용 구현

---
+++ coupon-service/src/main/java/com/palja/coupon_service/infrastructure/external/kafka/KafkaTopics.java
@@ -0,0 +1,7 @@
+package com.palja.coupon_service.infrastructure.external.kafka;
+
+public class KafkaTopics {
+    public static final String COUPON_USE_REQUEST = "order.coupon.use.request";
+    public static final String COUPON_USE_SUCCESS = "order.coupon.use.success";
+    public static final String COUPON_USE_FAILURE = "order.coupon.use.failure";
+}
