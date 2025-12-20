697190c8e1d4e1dc271d20366fe02944e7f6b7ea
newbee9507 <cho6185@naver.com>
2025-12-20 17:09:51 +0900

    테스트65

--- product-service/src/main/java/com/palja/product_service/presentation/controller/Impl/ProductControllerImpl.java
+++ product-service/src/main/java/com/palja/product_service/presentation/controller/Impl/ProductControllerImpl.java
@@ -33,7 +33,7 @@ public class ProductControllerImpl {
     @RequiredRole(UserRole.COMPANY_USER)
     @PostMapping()
     public ResponseEntity<ApiResponse<CreateProductRes>> createProduct(@RequestBody @Valid CreateProductReq req) {
-        System.out.println("adcdefghijklmnopqrdsdvcdfdfdddddddddddㅇfdd");
+        System.out.println("adcdefghijklmnopqrddsdvcdfdfdddddddddddㅇfdd");
         CreateProductCommand createCommand = req.toCommand(req);
         CreateProductRes res = service.createProduct(createCommand);
 
