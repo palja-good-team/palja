34ce526f3e6753a150b0762ea1acfe2f93b32fea
bitamin707 <bitamin707@naver.com>
2025-12-20 21:54:47 +0900

    feat:kafka Config 설정

--- coupon-service/src/main/java/com/palja/coupon_service/infrastructure/external/kafka/consumer/CouponKafkaConsumer.java
+++ coupon-service/src/main/java/com/palja/coupon_service/infrastructure/external/kafka/consumer/CouponKafkaConsumer.java
@@ -1,10 +1,11 @@
 package com.palja.coupon_service.infrastructure.external.kafka.consumer;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.palja.common.auditor.CurrentUser;
 import com.palja.coupon_service.application.command.UseCouponCommand;
+import com.palja.coupon_service.application.event.dto.request.CouponCancelEventReq;
 import com.palja.coupon_service.application.event.dto.request.CouponUseEventReq;
+import com.palja.coupon_service.application.event.dto.response.CouponCancelEventRes;
 import com.palja.coupon_service.application.event.dto.response.CouponUseEventRse;
 import com.palja.coupon_service.application.service.CouponService;
 import com.palja.coupon_service.infrastructure.external.kafka.KafkaTopics;
@@ -41,18 +42,46 @@ public class CouponKafkaConsumer {
 
             couponService.useCoupon(command);
 
-            CouponUseEventRse response = new CouponUseEventRse(event.getSagaId(), event.getOrderId());
+            CouponUseEventRse response = CouponUseEventRse.of(event.getSagaId(), event.getOrderId());
 
             kafkaTemplate.send(KafkaTopics.COUPON_USE_SUCCESS, response);
             log.info("쿠폰 사용 성공 - sagaId: {}, orderId: {}, couponUserId: {}",
                     event.getSagaId(), event.getOrderId(), event.getCouponUserId());
-        } catch (Exception e) {
-            log.error("쿠폰 사용 실패 - sagaId: {}, orderId: {}, couponUserId: {}",
-                    event.getSagaId(), event.getOrderId(), event.getCouponUserId());
 
-            CouponUseEventRse response = new CouponUseEventRse(event.getSagaId(), event.getOrderId());
+        } catch (Exception e) {
+            CouponUseEventRse response = CouponUseEventRse.of(event.getSagaId(), event.getOrderId());
 
             kafkaTemplate.send(KafkaTopics.COUPON_USE_FAILURE, response);
+
+            log.error("쿠폰 사용 실패 - sagaId: {}, orderId: {}, couponUserId: {}",
+                    event.getSagaId(), event.getOrderId(), event.getCouponUserId(), e);
         }
+    }
+
+    @KafkaListener(topics = KafkaTopics.COUPON_CANCEL_REQUEST)
+    public void cancelCoupon(ConsumerRecord<String, Object> record) {
+        CouponCancelEventReq event = objectMapper.convertValue(record.value(), CouponCancelEventReq.class);
+
+        log.info("쿠폰 취소 요청 이벤트 수신 - sagaId: {}, orderId: {}, couponUserId: {}",
+                event.getSagaId(), event.getOrderId(), event.getCouponUserId());
+
+        try {
+            couponService.cancelCoupon(event.getCouponUserId(), CurrentUser.getLoginId());
+
+            CouponCancelEventRes response = CouponCancelEventRes.of(event.getSagaId(), event.getOrderId());
+
+            kafkaTemplate.send(KafkaTopics.COUPON_CANCEL_SUCCESS, response);
+            log.info("쿠폰 취소 성공 - sagaId: {}, orderId: {}, couponUserId: {}",
+                    event.getSagaId(), event.getOrderId(), event.getCouponUserId());
+
+        } catch (Exception e) {
+            CouponCancelEventRes response = CouponCancelEventRes.of(event.getSagaId(), event.getOrderId());
+
+            kafkaTemplate.send(KafkaTopics.COUPON_CANCEL_FAILURE, response);
+
+            log.error("쿠폰 취소 실패 - sagaId: {}, orderId: {}, couponUserId: {}",
+                    event.getSagaId(), event.getOrderId(), event.getCouponUserId(), e);
+        }
+
     }
 }
