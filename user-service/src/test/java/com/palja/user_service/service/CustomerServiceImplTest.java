8ff8d918cdb65a542780e1c24412b9d971853c33
geon <leegeonhee0204@naver.com>
2025-12-23 10:50:47 +0900

    test: 모든 테스트 코드가 성공하도록 수정

--- user-service/src/test/java/com/palja/user_service/service/CustomerServiceImplTest.java
+++ user-service/src/test/java/com/palja/user_service/service/CustomerServiceImplTest.java
@@ -30,7 +30,7 @@ import com.palja.user_service.application.dto.response.CreateUserRes;
 import com.palja.user_service.application.dto.response.ReadCustomerDetailRes;
 import com.palja.user_service.application.dto.response.ReadCustomerSummaryRes;
 import com.palja.user_service.application.dto.response.UpdateCustomerDetailRes;
-import com.palja.user_service.application.port.ReviewClient;
+import com.palja.user_service.application.event.publisher.UserDomainEventPublisher;
 import com.palja.user_service.application.service.impl.CustomerServiceImpl;
 import com.palja.user_service.application.util.JwtUtil;
 import com.palja.user_service.domain.entity.User;
@@ -44,7 +44,7 @@ public class CustomerServiceImplTest {
 
 	@Mock private UserRepository userRepository;
 	@Mock private TokenRepository tokenRepository;
-	@Mock private ReviewClient reviewClient;
+	@Mock private UserDomainEventPublisher userDomainEventPublisher;
 	@Mock private PasswordEncoder passwordEncoder;
 	@Mock private JwtUtil jwtUtil;
 
@@ -522,8 +522,8 @@ public class CustomerServiceImplTest {
 			// then
 			assertThat(customer1.isDeleted()).isTrue();
 			then(userRepository).should(times(1)).findByLoginIdAndRoleAndDeletedAtIsNull(anyString(), any(UserRole.class));
-			then(tokenRepository).should(times(1)).save(anyString(), anyString(), anyLong());
-			then(tokenRepository).should(times(1)).remove(anyString());
+			then(tokenRepository).should(times(1)).addAccessTokenToBlackList(anyString(), anyString(), anyLong());
+			then(tokenRepository).should(times(1)).deleteRefreshToken(anyString());
 		}
 
 		@Nested
