c05ac53672393481824c680d37bc311795c5f0c0
김부경 <sa01023@naver.com>
2025-12-24 10:44:35 +0900

    feat: KafkaEvent 타입 계층 도입 및 Payment 이벤트 DTO 적용

--- order-service/src/main/java/com/palja/order_service/application/event/dto/PaymentEvent.java
+++ order-service/src/main/java/com/palja/order_service/application/event/dto/PaymentEvent.java
@@ -1,4 +1,10 @@
 package com.palja.order_service.application.event.dto;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.palja.order_service.application.event.dto.response.PaymentBaseEventRes;
+
+@JsonSubTypes({
+        @JsonSubTypes.Type(value = PaymentBaseEventRes.class, name = "PaymentBaseEventRes"),
+})
 public interface PaymentEvent extends KafkaEvent {
 }
