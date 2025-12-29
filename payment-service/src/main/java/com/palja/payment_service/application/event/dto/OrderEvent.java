01b41fde5fcdf07dcfaf5aab5b71c76ca6ccb5ec
김혜윤 <hhyeyun22@gmail.com>
2025-12-29 10:36:31 +0900

    refactor: PaymentService Kafka 역직렬화 수정 (#348)

--- payment-service/src/main/java/com/palja/payment_service/application/event/dto/OrderEvent.java
+++ payment-service/src/main/java/com/palja/payment_service/application/event/dto/OrderEvent.java
@@ -3,10 +3,13 @@ package com.palja.payment_service.application.event.dto;
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.palja.payment_service.application.event.dto.request.PaymentCancelEventReq;
 import com.palja.payment_service.application.event.dto.request.PaymentCreateEventReq;
+import com.palja.payment_service.application.event.dto.response.PaymentCreateEventRes;
 
 @JsonSubTypes({
         @JsonSubTypes.Type(value = PaymentCreateEventReq.class, name = "PaymentCreateEventReq"),
-        @JsonSubTypes.Type(value = PaymentCancelEventReq.class, name = "PaymentCancelEventReq")
+        @JsonSubTypes.Type(value = PaymentCancelEventReq.class, name = "PaymentCancelEventReq"),
+
+        @JsonSubTypes.Type(value = PaymentCreateEventRes.class, name = "PaymentCreateEventRes")
 })
 public interface OrderEvent extends KafkaEvent {
 }
--- payment-service/src/main/java/com/palja/payment_service/application/event/dto/PaymentEvent.java
+++ payment-service/src/main/java/com/palja/payment_service/application/event/dto/PaymentEvent.java
@@ -4,16 +4,11 @@ import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.palja.payment_service.application.event.dto.request.*;
 
-@JsonTypeInfo(
-        use = JsonTypeInfo.Id.NAME,
-        include = JsonTypeInfo.As.PROPERTY,
-        property = "@type"
-)
 @JsonSubTypes({
         @JsonSubTypes.Type(value = PaymentApprovedEventReq.class, name = "PaymentApprovedEventReq"),
         @JsonSubTypes.Type(value = PaymentCanceledEventReq.class, name = "PaymentCanceledEventReq"),
         @JsonSubTypes.Type(value = PaymentFailedEventReq.class, name = "PaymentFailedEventReq"),
         @JsonSubTypes.Type(value = PaymentCancelFailedEventReq.class, name = "PaymentCancelFailedEventReq")
 })
-public interface PaymentEvent {
+public interface PaymentEvent extends KafkaEvent {
 }
